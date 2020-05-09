5be30ffa8b14c434a129475600996a019eb9f41b
atanasiliev <iliev.i.atanas@gmail.com>
2020-05-09 11:33:14 +0300

    Skeleton added.

---
+++ Survello/Survello.Database/SurvelloContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Survello.Database
+{
+    public class SurvelloContext
+    {
+    }
+}
---
+++ Survello/Survello.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Survello.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
