6d5d8c1e9ae1fcbe2eaf242afb69db65907d55f4
YoannaKostova <yoanna.kostova@yahoo.com>
2020-05-10 21:13:17 +0300

    Up to date version of the app

--- Survello/Survello.Web/Startup.cs
+++ Survello/Survello.Web/Startup.cs
@@ -56,6 +56,8 @@ namespace Survello.Web
             services.AddRazorPages();
 
             services.AddSingleton<IEmailSender, EmailSender>();
+
+            services.AddServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
