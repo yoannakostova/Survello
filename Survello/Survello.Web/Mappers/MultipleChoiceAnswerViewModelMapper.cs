fee5af237a2ea66c420f78fbe32f262b0ec4ef5a
YoannaKostova <yoanna.kostova@yahoo.com>
2020-05-11 12:24:43 +0300

    ViewModels and Mappers added

---
+++ Survello/Survello.Web/Mappers/MultipleChoiceAnswerViewModelMapper.cs
@@ -0,0 +1,51 @@
+﻿using Survello.Services.ConstantMessages;
+using Survello.Services.DTOEntities;
+using Survello.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Survello.Web.Mappers
+{
+    public static class MultipleChoiceAnswerViewModelMapper
+    {
+        public static MultipleChoiceAnswerDTO MapFrom(this MultipleChoiceAnswerViewModel viewModel)
+        {
+            if (viewModel == null)
+            {
+                throw new Exception(ExceptionMessages.EntityNull);
+            }
+
+            return new MultipleChoiceAnswerDTO
+            {
+                Id = viewModel.Id,
+                MultipleChoiceQuestionId = viewModel.MultipleChoiceQuestionId
+            };
+        }
+
+        public static MultipleChoiceAnswerViewModel MapFrom(this MultipleChoiceAnswerDTO dto)
+        {
+            if (dto == null)
+            {
+                throw new Exception(ExceptionMessages.EntityNull);
+            }
+
+            return new MultipleChoiceAnswerViewModel
+            {
+                Id = dto.Id,
+                MultipleChoiceQuestionId = dto.MultipleChoiceQuestionId
+            };
+        }
+
+        public static ICollection<MultipleChoiceAnswerViewModel> MapFrom(this ICollection<MultipleChoiceAnswerDTO> dtos)
+        {
+            return dtos.Select(MapFrom).ToList();
+        }
+
+        public static ICollection<MultipleChoiceAnswerDTO> MapFrom(this ICollection<MultipleChoiceAnswerViewModel> viewModels)
+        {
+            return viewModels.Select(MapFrom).ToList();
+        }
+    }
+}
---
+++ Survello/Survello.Web/Models/MultipleChoiceAnswerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Survello.Web.Models
+{
+    public class MultipleChoiceAnswerViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid MultipleChoiceQuestionId { get; set; }
+    }
+}
