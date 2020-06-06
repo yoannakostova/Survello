ee97e22e5497bb4e53760b3c91959193bd6d64df
Yoanna Kostova <yoanna.kostova@yahoo.com>
2020-06-06 14:33:50 +0000

    Updated views of the application. All buttons checked. New images added.

---
+++ Survello/Survello.Web/wwwroot/js/back-to-top.js
@@ -0,0 +1,14 @@
+﻿btn = $('#buttonBackToTop');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
